52f507abe088d5acf17c5f4b25e01d83c813ac25
DiosOne <domaone83@proton.me>
2025-09-12 23:35:42 +1000

    changed wsgi to fix module error

--- library_db_project/wsgi.py
+++ library_db_project/wsgi.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add the root of the project to the Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
 from api.app import app
 
 if __name__ == "__main__":
-    app.run()
+    # Only for local development
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
